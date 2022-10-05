68f9939a32219a480e7a8b64164b49a4a5103189
Erol Dalkılıç <eroldalg@gmail.com>
2022-10-05 23:28:00 +0300

    YeniCagri

--- MvcFirmaCagri/Controllers/DefaultController.cs
+++ MvcFirmaCagri/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcFirmaCagri.Models.entity;
 
 namespace MvcFirmaCagri.Controllers
 {
@@ -13,10 +14,33 @@ namespace MvcFirmaCagri.Controllers
         {
             return View();
         }
-
+        DbisTakipEntities db = new DbisTakipEntities();
         public ActionResult AktifCagrilar()
         {
+            var cagrilar = db.TBLCagrilar.Where(x=>x.Durum==true &&
+            x.CagriFirma == 12).ToList();
+            return View(cagrilar);
+        }
+        public ActionResult PasifCagrilar()
+        {
+            var cagrilar = db.TBLCagrilar.Where(x => x.Durum == false &&
+            x.CagriFirma == 12).ToList();
+            return View(cagrilar);
+        }
+        [HttpGet]
+        public ActionResult YeniCagri()
+        {
             return View();
+        } 
+        [HttpPost]
+        public ActionResult YeniCagri(TBLCagrilar p)
+        {
+            p.Durum = true;
+            p.Tarih= DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CagriFirma = 12;
+          db.TBLCagrilar.Add(p);
+          db.SaveChanges();
+          return RedirectToAction("AktifCagrilar");
         }
 
     }
